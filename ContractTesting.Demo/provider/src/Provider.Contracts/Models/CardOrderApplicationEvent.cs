35788c28905cf0b0f4eff16536ca428576d3664a
Niyaz Fattakhov <niyazambiti.on@mail.ru>
2024-05-13 13:52:29 +0300

    #adds rabbitmq interaction

---
+++ ContractTesting.Demo/provider/src/Provider.Contracts/Models/CardOrderApplicationEvent.cs
@@ -0,0 +1,11 @@
+﻿namespace Provider.Contracts.Models;
+
+public class CardOrderApplicationEvent
+{
+    /// <summary> Идентификатор пользователя /// </summary>
+    public string UserId { get; set; }
+    /// <summary> Код карточного продукта /// </summary>
+    public string CardCode { get; set; }
+    /// <summary> Дата оформления заявления /// </summary>
+    public DateTime ApplicationDate { get; set; }
+}
--- ContractTesting.Demo/provider/src/Provider.Host/Startup.cs
+++ ContractTesting.Demo/provider/src/Provider.Host/Startup.cs
@@ -1,14 +1,26 @@
-﻿using Provider.Domain;
-
+﻿using EasyNetQ;
+using EasyNetQ.DI;
+using Provider.Contracts.Models;
+using Provider.Domain;
 namespace Provider.Host;
 
 public class Startup
 {
     public IConfiguration Configuration { get; }
-    
+
     public Startup(IConfiguration configuration)
     {
         Configuration = configuration;
+    }
+
+    class SimpleTypeNameSerializer : ITypeNameSerializer
+    {
+        public string Serialize(Type type) => type.Name;
+
+        public Type DeSerialize(string typeName)
+        {
+            return Type.GetType("Provider.Contracts.Models.CardOrderApplicationEvent, Provider.Contracts");
+        }
     }
     
     public void ConfigureServices(IServiceCollection services)
@@ -16,8 +28,23 @@ public class Startup
         services.AddMvc().AddControllersAsServices();
         services.AddScoped<ICardAccountsRepository, CardAccountsRepository>();
         services.AddSwaggerGen();
+        
+        var advanced = RabbitHutch.CreateBus("host=localhost:5672;username=guest;password=guest", s =>
+        {
+            s.EnableConsoleLogger();
+            s.EnableSystemTextJson();
+            s.Register<ITypeNameSerializer, SimpleTypeNameSerializer>();
+        }).Advanced;
+        var exchange = advanced.ExchangeDeclare("SpecialExchangeName", "direct");
+        var queue = advanced.QueueDeclare("SpecialQueueName");
+        advanced.Bind(exchange, queue, routingKey: "super-routing-key");
+        advanced.Consume<CardOrderApplicationEvent>(queue, (message, _) =>
+            Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine($"Новый заказ: {message.Body.UserId}, {message.Body.CardCode}, {message.Body.ApplicationDate}");
+            }));
     }
-    
+
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
         if (env.IsDevelopment())
@@ -27,9 +54,6 @@ public class Startup
         }
 
         app.UseRouting();
-        app.UseEndpoints(endpoints =>
-        {
-            endpoints.MapControllers();
-        });
+        app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
     }
 }
