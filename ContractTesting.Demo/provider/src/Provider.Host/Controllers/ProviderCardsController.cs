bd2f87582d27e9c47f0b7283906af3173a48a218
Niyaz Fattakhov <niyazambiti.on@mail.ru>
2024-07-16 14:03:03 +0300

    #query param delete check

--- ContractTesting.Demo/provider/src/Provider.Host/Controllers/ProviderCardsController.cs
+++ ContractTesting.Demo/provider/src/Provider.Host/Controllers/ProviderCardsController.cs
@@ -7,6 +7,46 @@ using Provider.Host.Helpers;
 
 namespace Provider.Host.Controllers;
 
+/*
+ *
+ *{
+  "providerName": "Demo.Provider",
+  "providerApplicationVersion": "1.1.0",
+  "success": false,
+  "verificationDate": "2024-07-16T10:59:09+00:00",
+  "testResults": [{
+      "interactionId": "45f04c85450a74e86ee3532f64fc610fa62b3410",
+      "success": true
+    }, {
+      "interactionId": "a34f508ad02d3fdc02128a8bc1d17423121a2155",
+      "success": true
+    }, {
+      "interactionId": "f94fb21777492253d725d93eef28d2f8344f44aa",
+      "mismatches": [{
+          "attribute": "body",
+          "description": "Expected body Present(89 bytes, application/json) but was empty",
+          "identifier": "/"
+        }, {
+          "attribute": "status",
+          "description": "expected 200 but was 404"
+        }, {
+          "attribute": "header",
+          "description": "Expected a header 'Content-Type' but was missing",
+          "identifier": "Content-Type"
+        }],
+      "success": false
+    }, {
+      "interactionId": "e4304a59a374b803855951c157a7282e9302bfc5",
+      "success": true
+    }],
+  "verifiedBy": {
+    "implementation": "Pact-Rust",
+    "version": "1.1.19"
+  }
+}
+ * 
+ */
+
     [Route("api/provider/cards")]
     public class ProviderCardsController : ControllerBase
     {
@@ -36,10 +76,9 @@ namespace Provider.Host.Controllers;
         /// <param name="accountId">Идентификатор счёта</param>
         /// <param name="request">Тело запроса</param>
         [HttpPost("{userId}")]
-        public async Task<ActionResult<CardInfoResponse>> CreateCardOrder(string userId, 
-            [FromQuery] string accountId, [FromBody] CreateCardOrderRequest request)
+        public async Task<ActionResult<CardInfoResponse>> CreateCardOrder(string userId, [FromBody] CreateCardOrderRequest request)
         {
-            var result = await _cardAccountsRepository.AddCard(userId, accountId, request.IsNamed);
+            var result = await _cardAccountsRepository.AddCard(userId, "acidef8ef642-3cab-4f70-9c12-c9757e698ad1", request.IsNamed);
             return result != null ? Ok(MapperExtensions.MapCardInfo(result)) : NotFound();
         }
         
